bc835a5cbd1156f2072dfa8b78cdb496d112c6e9
Diego Lima <lima.j.diego@gmail.com>
2024-04-24 14:00:36 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+options = '''
+Bem-vindo ao banco .py
+
+Escolha uma opção:
+
+1- Depositar
+2- Saque
+3- Extrato
+4- Encerrar
+'''
+
+deposits = []
+
+while True:
+    opt_chosen = input(options)
+
+   
+        
